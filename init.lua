bfa5e0ecfbcf42923e964052c251bbd45b5f55b2
Mir Sole <65946597+ZacIsBurnt@users.noreply.github.com>
2022-02-22 12:41:45 +1100

    me when sirmole

--- init.lua
+++ init.lua
@@ -2,6 +2,8 @@
 
 local nxml = dofile_once("mods/subnoitica/lib/nxml.lua")
 
+BiomeMapLoadImage( 0, 0, "mods/subnoitica/files/biome_map.png" )
+
 function OnModInit()
 	print("Mod - OnModInit()")
 end
@@ -25,5 +27,7 @@ end
 function Moulding()
 	
 end
+
+ModMagicNumbersFileAdd( "mods/subnoitica/files/magic_numbers.xml" ) 
 
 GamePrint("Subnoitica")
