b36c65748a6a6da9b31185a018453d5ae9c75947
Mir Sole <65946597+ZacIsBurnt@users.noreply.github.com>
2022-02-23 17:31:05 +1100

    a

---
+++ data/scripts/biomes/kelp_forest.lua
@@ -0,0 +1,59 @@
+-- default biome functions that get called if we can't find a a specific biome that works for us
+CHEST_LEVEL = 3
+dofile_once("data/scripts/director_helpers.lua")
+dofile_once("data/scripts/biome_scripts.lua")
+
+RegisterSpawnFunction( 0xffffeedd, "init" )
+RegisterSpawnFunction( 0xffb4a00a, "spawn_fish" )
+RegisterSpawnFunction( 0xff9797e8, "spawn_fragments" )
+
+g_fish =
+{
+	total_prob = 0,
+	-- add skullflys after this step
+	{
+		prob   		= 0.3,
+		min_count	= 3,
+		max_count	= 5,    
+		entity 	= "data/entities/animals/peeper/peeper.xml"
+	},
+
+	{
+		prob   		= 0.25,
+		min_count	= 2,
+		max_count	= 6,    
+		entity 	= "data/entities/animals/bladderfish/bladderfish.xml"
+	},
+
+	{
+		prob   		= 0.03,
+		min_count	= 1,
+		max_count	= 1,    
+		entity 	= "data/entities/animals/peeper/peeper.xml"
+	},
+}
+
+g_fragments =
+{
+	total_prob = 0,
+	-- add skullflys after this step
+	{
+		prob   		= 0.3,
+		min_count	= 1,
+		max_count	= 1,   
+		entity 	= "data/entities/fragments/fragment_seaglide.xml"
+	},
+
+}
+
+function init( x, y, w, h )
+
+end
+
+function spawn_fish(x, y)
+	spawn(g_fish,x,y)
+end
+
+function spawn_fragments(x, y)
+	spawn(g_fragments,x,y)
+end
