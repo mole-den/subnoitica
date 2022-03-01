f8d4082496e6505032897799fbc419ff55f762f1
Oliver Fox-Smith <ofoxsmith@outlook.com>
2022-03-01 12:18:16 +1100

    add material collection and rename material entity tags

--- data/entities/collect_material.lua
+++ data/entities/collect_material.lua
@@ -8,9 +8,12 @@ function collision_trigger(colliding_entity_id)
     end
     local store = stringstore.open_store(stringstore.noita.variable_storage_components(
         EntityGetWithTag("player_unit")[1])).materials
-        
-    local material = (string.gmatch(string.gmatch((string.gmatch(EntityGetTags(colliding_entity_id),
-        'material_[^,| ]+'))(), '_[^,| ]+')(), '[^_]+')())
-    
-    
+
+    local material = (string.gmatch(string.gmatch(
+        (string.gmatch(EntityGetTags(colliding_entity_id), 'material_[^,| ]+'))(), '_[^,| ]+')(), '[^_]+')())
+    if store[material] == nil then
+        store[material] = {count = 0}
+    end
+    store[material].count = store[material].count + 1
+    return
 end
