d1a945a6006dfa739845547f2b1fd6a50a04ba3b
Oliver Fox-Smith <ofoxsmith@outlook.com>
2022-03-02 13:05:29 +1100

    add recipies file

---
+++ data/entities/crafting.lua
@@ -0,0 +1,4 @@
+dofile_once("data/scripts/lib/utilities.lua")
+dofile("mods/subnoitica/lib/stringstore.lua")
+dofile("mods/subnoitica/lib/noitavariablestore.lua")
+local rec
---
+++ data/entities/recipies.lua
@@ -0,0 +1,15 @@
+return {
+    seaglide = {
+        materials = {
+            titanium = 3,
+            gold = 2,
+        },
+        creates = "data/entities/items/seaglide.xml"
+    },
+    knife = {
+        materials = {
+            titanium = 2,
+        },
+        creates = "data/entities/items/survival_knife.xml"
+    }
+}
