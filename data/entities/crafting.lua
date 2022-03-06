d9cac3a14853071cde58d6fc2e2619cbcb01bc4d
Oliver Fox-Smith <ofoxsmith@outlook.com>
2022-03-07 09:00:42 +1100

    add crafting function (no gui yet)

--- data/entities/crafting.lua
+++ data/entities/crafting.lua
@@ -2,4 +2,21 @@ dofile_once("data/scripts/lib/utilities.lua")
 dofile_once("mods/subnoitica/lib/stringstore.lua")
 dofile_once("mods/subnoitica/lib/noitavariablestore.lua")
 recipies = dofile_once("data/entities/recipies.lua")
-gui = GuiCreate()
+store = stringstore.open_store(stringstore.noita.variable_storage_components(
+    EntityGetWithTag("player_unit")[1])).materials
+
+function craft(item)
+    local i = recipies[item]
+    for k, v in pairs(i.materials) do
+        if (type(store[k]) == "string") then
+            store[k] = 0
+        end
+        if (store[k] < v) then return end
+    end
+    for k, v in pairs(i.materials) do 
+        store[k] = store[k] - v
+    end
+    local x, y = EntityGetTransform(EntityGetWithTag("player_unit")[1])
+    EntityLoad(i.creates, x, y)
+    GamePrint("Crafted " .. item .. "!")
+end
--- init.lua
+++ init.lua
@@ -1,24 +1,24 @@
-ModLuaFileAppend( "data/scripts/gun/gun_actions.lua", "mods/subnoitica/files/actions.lua" )
-ModLuaFileAppend( "data/scripts/perks/perk_list.lua", "mods/subnoitica/files/perk_list.lua")
+ModLuaFileAppend("data/scripts/gun/gun_actions.lua", "mods/subnoitica/files/actions.lua")
+ModLuaFileAppend("data/scripts/perks/perk_list.lua", "mods/subnoitica/files/perk_list.lua")
 ModMaterialsFileAdd("mods/subnoitica/files/materials.xml")
 
 dofile("mods/subnoitica/lib/stringstore.lua")
 dofile("mods/subnoitica/lib/noitavariablestore.lua")
 local nxml = dofile_once("mods/subnoitica/lib/nxml.lua")
-BiomeMapLoadImage( 0, 0, "mods/subnoitica/files/biome_map.png" )
+BiomeMapLoadImage(0, 0, "mods/subnoitica/files/biome_map.png")
 
 function OnModInit()
-	print("Mod - OnModInit()")
+    print("Mod - OnModInit()")
 end
 
 function OnModPostInit()
-	print("Mod - OnModPostInit()")
+    print("Mod - OnModPostInit()")
 end
 
-function OnPlayerSpawned( player_entity )
-	local x = stringstore.open_store(stringstore.noita.variable_storage_components(EntityGetWithTag("player_unit")[1]))
-	x.fragments = {}
-	x.materials = {}
+function OnPlayerSpawned(player_entity)
+    local x = stringstore.open_store(stringstore.noita.variable_storage_components(EntityGetWithTag("player_unit")[1]))
+    x.fragments = {}
+    x.materials = {}
 end
 
 function OnWorldPostUpdate()
@@ -26,9 +26,9 @@ function OnWorldPostUpdate()
 end
 
 function OnPlayerDied()
-	print("dead")
+    print("dead")
 end
 
-ModMagicNumbersFileAdd( "mods/subnoitica/files/magic_numbers.xml" ) 
+ModMagicNumbersFileAdd("mods/subnoitica/files/magic_numbers.xml")
 
 GamePrint("Subnoitica")
